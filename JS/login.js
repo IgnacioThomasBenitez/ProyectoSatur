f7a7f3e44579c48a57674a251fffcc41e4cd61ad
IgnacioThomasBenitez <benitezignacio46@gmail.com>
2023-07-01 16:59:58 -0300

    Nuevo

---
+++ JS/login.js
@@ -0,0 +1,9 @@
+document.getElementById('loginButton').addEventListener('click', function() {
+    window.location.href = '../HTML/IngresarVendedor.html';
+});
+
+document.getElementById('pag').addEventListener('click', function() {
+    window.location.href = '../Venta/index.html';
+});
+  
+
---
+++ JS/volver.js
@@ -0,0 +1,4 @@
+document.getElementById('compra').addEventListener('click', function() {
+    window.location.href = '../HTML/Ingresar.html';
+  });
+  
